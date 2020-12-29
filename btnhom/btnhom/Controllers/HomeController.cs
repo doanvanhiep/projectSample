248348c26d5b0247cdd5e003087fdd2cad5ca3c0
doanvanhiep <doanvanhiepebn951@gmail.com>
2020-12-29 08:29:49 +0700

    commit first

---
+++ btnhom/btnhom/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace btnhom.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ThanhVien()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult DeTai()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
